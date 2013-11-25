32f77a81999103a36c8e3a6613aac31b93117e60
diego <diego@diego-G50VT>
2013-11-24 21:06:00 -0500

    refactoring TestUndoRedoStack

--- trunk/UndoRedoStack/src/test/java/com/github/undoredostack/TestUndoRedoStack.java
+++ trunk/UndoRedoStack/src/test/java/com/github/undoredostack/TestUndoRedoStack.java
@@ -1,16 +1,22 @@
 package com.github.undoredostack;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestUndoRedoStack {
 
     static final String COMMAND_1 = "command1";
+    private UndoRedoStack stack;
+
+    @Before
+    public void setUp() {
+        stack = new UndoRedoStack();
+    }
 
     @Test
     public void undoReturnLastCommand() {
         //Arrange
-        UndoRedoStack stack = new UndoRedoStack();
         stack.command(COMMAND_1);
         //Act
         String actualResult = stack.undo();
